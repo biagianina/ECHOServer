4ac553ee1de4b0c29cea86ff01226b5e3e154e11
biagianina <53540842+biagianina@users.noreply.github.com>
2019-11-16 11:06:29 +0200

    client correct

--- EchoClient/Program.cs
+++ EchoClient/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
 namespace EchoClient
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            TcpClient client = new TcpClient("localhost", 123);
+
+            string message = Console.ReadLine();
+
+            byte[] sendData = Encoding.ASCII.GetBytes(message);
+
+            NetworkStream stream = client.GetStream();
+
+            stream.Write(sendData);
+
+            stream.Close();
+            client.Close();
         }
     }
 }
